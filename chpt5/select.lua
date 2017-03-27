21e35842f2b94419d63734aa6a6a257eacfea7b1
zaifeng <20012382@qq.com>
2017-03-27 11:37:15 +0800

    update select example

--- chpt5/select.lua
+++ chpt5/select.lua
@@ -1,12 +1,8 @@
--- wrong example
-do
-    function sel(...)
-        for i=1, select('#', ...) do
-            local arg = select(i, ...)
-            print(arg)
-        end
+function sel(...)
+    for i=1, select('#', ...) do
+        local arg = select(i, ...)
+        print(arg)
     end
-
-    sel(1,2,3,4,5,6)
 end
 
+sel(1,2,3,4,5,6)
