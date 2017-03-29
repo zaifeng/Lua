910aa7c0f1c730b7aefe613f5bf8910875c2688d
zaifeng <20012382@qq.com>
2017-03-29 10:03:30 +0800

    update example

--- chpt3/table.lua
+++ chpt3/table.lua
@@ -1,11 +1,34 @@
-a = {x=1,y=2 ; ["z"]=5,["t"]=6 ; "sina","360","letv"}
+a = {x=1,y=2 ; ["z"]=5,["t"]=6 ; "sina","360","letv",}
 
-
-for i,v in pairs(a) do
-    print(v)
+-- nodigit key output randomly
+for k,_ in pairs(a) do
+    print(a[k])
 end
-
--- nil nothing output
+print("--------")
+-- if digit key 1 not set nothing will be output
 for i=1,#a do
-    -- print(a[i])
+    print(a[i])
 end
+
+print("--------")
+-- like upon
+for i,_ in ipairs(a) do
+    print(a[i])
+end
+
+-- Output
+-- sina
+-- 360
+-- letv
+-- 2
+-- 5
+-- 6
+-- 1
+--------
+-- sina
+-- 360
+-- letv
+--------
+-- sina
+-- 360
+-- letv
