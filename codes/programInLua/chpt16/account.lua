32c68a4ff7ffdc7aa35ebde62675028fd34cfd2f
zaifeng <20012382@qq.com>
2017-04-07 13:56:05 +0800

    add oop example

--- codes/programInLua/chpt16/account.lua
+++ codes/programInLua/chpt16/account.lua
@@ -9,9 +9,20 @@ end
 function Account.withdraw(self, v)
     self.balance = self.balance - v
 end
+
 a = Account
 Account = nil
 
 -- a.withdraw(10) -- wrong revoke
 
 a.withdraw(a,10)
+
+newAccount = {balance = 10}
+function newAccount:withdraw(v)
+    self.balance = self.balance - v
+end
+
+function newAccount.deposit(m)
+    self.balance = self.balance + m
+end
+
