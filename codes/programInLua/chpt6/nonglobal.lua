451582159a4bcc41d00be606a6bef9b535e4dba1
zaifeng <20012382@qq.com>
2017-04-11 15:22:05 +0800

    lua oop study

--- codes/programInLua/chpt6/nonglobal.lua
+++ codes/programInLua/chpt6/nonglobal.lua
@@ -1,8 +1,14 @@
 lib = {p1=0,p2=0}
 
-function lib:new(x,y)
+function lib:new()
+    o = o or {}
+    setmetatable(o,self)
+    self.__index = self
+    return o
+end
+
+function lib:init(x,y)
     self.p1, self.p2 = x , y
-    -- print(self.p1,self.p2)
 end
 
 function lib:print()
@@ -14,8 +20,8 @@ function lib:sadd()
     return self.p1 + self.p2
 end
 
-a = lib
-a:new(10,20)
+a = lib:new()
+a:init(10,20)
 a:print()
 
-print(lib:sadd())
+print(a:sadd())
