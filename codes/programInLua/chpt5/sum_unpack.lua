545f64b462793a6f0ae0cea9386cd6eab5858e37
zaifeng <20012382@qq.com>
2017-06-07 17:21:52 +0800

    update lua example

--- codes/programInLua/chpt5/sum_unpack.lua
+++ codes/programInLua/chpt5/sum_unpack.lua
@@ -8,3 +8,23 @@ local function nsum(...)
       end
 
 print(nsum(table.unpack(nums)))
+
+-- nil in params
+
+local nnums = {1,2,3,4,5,6,nil,7,8,9,10}
+
+print(nsum(table.unpack(nnums))) -- 21 = 1+2+3+4+5+6
+
+function nsum1(...)
+    local sum = 0
+
+    for i=1, select('#', ...) do
+        local arg = select(i, ...)
+        if arg ~= nil then
+            sum = sum + arg
+        end
+    end
+    return sum
+end
+
+print(nsum1(table.unpack(nnums)))
